5782a66b995d3468f75394af0755432e29075e32
Ryan Leverenz <Ryan@DreamToaster.com>
2023-02-12 17:16:19 -0600

    stubbing out not yet implement stuff

--- steam_multiplayer_peer.cpp
+++ steam_multiplayer_peer.cpp
@@ -1,53 +1,88 @@
 #include "steam_multiplayer_peer.h"
 
 int SteamMultiplayerPeer::get_available_packet_count() const {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_available_packet_count not yet implemented");
     return -1;
 }
 
 Error SteamMultiplayerPeer::get_packet(const uint8_t **r_buffer, int &r_buffer_size) {
-	return Error();
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::put_packet not yet implemented");
+	ERR_FAIL_COND_V_MSG(incoming_packets.size() == 0, ERR_UNAVAILABLE, "No incoming packets available.");
+
+	current_packet = incoming_packets.front()->get();
+	incoming_packets.pop_front();
+
+	// *r_buffer = (const uint8_t *)(current_packet.packet->data);
+	// r_buffer_size = current_packet.packet->dataLength;
+
+	return OK;
 }
 
 Error SteamMultiplayerPeer::put_packet(const uint8_t *p_buffer, int p_buffer_size) {
-	return Error();
-}
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::put_packet not yet implemented");
+	return Error::ERR_PRINTER_ON_FIRE;
+;}
 
 int SteamMultiplayerPeer::get_max_packet_size() const {
-	return 0;
+	return 1 << 24;//from ENet
 }
 
 void SteamMultiplayerPeer::set_target_peer(int p_peer_id) {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::set_target_peer not yet implemented");
 }
 
 int SteamMultiplayerPeer::get_packet_peer() const {
-	return 0;
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_peer not yet implemented");
+	return -1;
 }
 
 SteamMultiplayerPeer::TransferMode SteamMultiplayerPeer::get_packet_mode() const {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_mode not yet implemented");
 	return TransferMode::TRANSFER_MODE_RELIABLE;
 }
 
 int SteamMultiplayerPeer::get_packet_channel() const {
-	return 0;
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_channel not yet implemented");
+	return -1;
 }
 
 void SteamMultiplayerPeer::disconnect_peer(int p_peer, bool p_force) {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::disconnect_peer not yet implemented");
 }
 
 bool SteamMultiplayerPeer::is_server() const {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::is_server not yet implemented");
 	return false;
 }
 
 void SteamMultiplayerPeer::poll() {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::poll not yet implemented");
 }
 
 void SteamMultiplayerPeer::close() {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::close not yet implemented");
 }
 
 int SteamMultiplayerPeer::get_unique_id() const {
-	return 0;
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_unique_id not yet implemented");
+	// ERR_FAIL_COND_V_MSG(!_is_active(), 0, "The multiplayer instance isn't currently active.");
+	return unique_id;
 }
 
 SteamMultiplayerPeer::ConnectionStatus SteamMultiplayerPeer::get_connection_status() const {
-	return ConnectionStatus::CONNECTION_DISCONNECTED;
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_connection_status not yet implemented");
+	return connection_status;
+}
+
+Error SteamMultiplayerPeer::create_server(int max_players) {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::create_server not yet implemented");
+	unique_id = 1;
+	return Error();
+}
+
+Error SteamMultiplayerPeer::create_client() {
+	ERR_PRINT("ERROR:: SteamMultiplayerPeer::create_client not yet implemented");
+	unique_id = generate_unique_id();
+
+	return Error();
 }
--- steam_multiplayer_peer.h
+++ steam_multiplayer_peer.h
@@ -8,6 +8,8 @@ class SteamMultiplayerPeer : public MultiplayerPeer {
 	GDCLASS(SteamMultiplayerPeer, MultiplayerPeer);
 
 public:
+
+	//MultiplayerPeer stuff
 	virtual int get_available_packet_count() const override;
 	virtual Error get_packet(const uint8_t **r_buffer, int &r_buffer_size) override;
 	virtual Error put_packet(const uint8_t *p_buffer, int p_buffer_size) override;
@@ -23,7 +25,22 @@ public:
 	virtual void close() override;
 	virtual int get_unique_id() const override;
 	virtual ConnectionStatus get_connection_status() const override;
+
+private:
+	uint32_t unique_id = 0;
+	ConnectionStatus connection_status;
+	struct Packet {
+		//  *packet = nullptr;
+		int from = 0;
+		int channel = 0;
+		TransferMode transfer_mode = TRANSFER_MODE_RELIABLE;
+	};
+	Packet current_packet;
+	List<Packet> incoming_packets;
+
+public:
+	Error create_server(int max_players);
+	Error create_client();//todo function signature here!
 };
 
 #endif
-
