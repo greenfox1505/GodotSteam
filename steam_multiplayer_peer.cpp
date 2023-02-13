1307dab57f672d1c216ca47008d65f085fcb969d
Ryan Leverenz <Ryan@DreamToaster.com>
2023-02-12 21:23:06 -0600

    more structure

--- steam_multiplayer_peer.cpp
+++ steam_multiplayer_peer.cpp
@@ -1,8 +1,45 @@
 #include "steam_multiplayer_peer.h"
 
+
+
+#include "godotsteam.h"
+
+
+#define MAX_STEAM_PACKET_SIZE k_cbMaxSteamNetworkingSocketsMessageSizeSend
+
+
+VARIANT_ENUM_CAST(SteamMultiplayerPeer::LOBBY_TYPE);
+VARIANT_ENUM_CAST(SteamMultiplayerPeer::CHAT_CHANGE);
+VARIANT_ENUM_CAST(SteamMultiplayerPeer::LOBBY_STATE);
+
+
+void SteamMultiplayerPeer::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("create_server","max_players"), &SteamMultiplayerPeer::create_server,DEFVAL(32));
+	ClassDB::bind_method(D_METHOD("create_client"), &SteamMultiplayerPeer::create_client);
+
+	BIND_ENUM_CONSTANT(PRIVATE);
+	BIND_ENUM_CONSTANT(FRIENDS_ONLY);
+	BIND_ENUM_CONSTANT(PUBLIC);
+	BIND_ENUM_CONSTANT(INVISIBLE);
+	BIND_ENUM_CONSTANT(PRIVATE_UNIQUE);
+
+
+	BIND_ENUM_CONSTANT(ENTERED);
+	BIND_ENUM_CONSTANT(LEFT);
+	BIND_ENUM_CONSTANT(DISCONNECTED);
+	BIND_ENUM_CONSTANT(KICKED);
+	BIND_ENUM_CONSTANT(BANNED);
+
+	BIND_ENUM_CONSTANT(DISCONNECTED);
+	BIND_ENUM_CONSTANT(HOST_PENDING);
+	BIND_ENUM_CONSTANT(HOST);
+	BIND_ENUM_CONSTANT(CLIENT_PENDING);
+	BIND_ENUM_CONSTANT(CLIENT);
+	
+}
+
 int SteamMultiplayerPeer::get_available_packet_count() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_available_packet_count not yet implemented");
-    return -1;
+	return incoming_packets.size();
 }
 
 Error SteamMultiplayerPeer::get_packet(const uint8_t **r_buffer, int &r_buffer_size) {
@@ -24,26 +61,35 @@ Error SteamMultiplayerPeer::put_packet(const uint8_t *p_buffer, int p_buffer_siz
 ;}
 
 int SteamMultiplayerPeer::get_max_packet_size() const {
-	return 1 << 24;//from ENet
+	return MAX_STEAM_PACKET_SIZE;//from ENet
 }
 
 void SteamMultiplayerPeer::set_target_peer(int p_peer_id) {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::set_target_peer not yet implemented");
+	target_peer = p_peer_id;
 }
 
 int SteamMultiplayerPeer::get_packet_peer() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_peer not yet implemented");
-	return -1;
+	ERR_FAIL_COND_V_MSG(!_is_active(), 1, "The multiplayer instance isn't currently active.");
+	ERR_FAIL_COND_V(incoming_packets.size() == 0, 1);
+
+	return incoming_packets.front()->get().from;
 }
 
 SteamMultiplayerPeer::TransferMode SteamMultiplayerPeer::get_packet_mode() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_mode not yet implemented");
-	return TransferMode::TRANSFER_MODE_RELIABLE;
+	ERR_FAIL_COND_V_MSG(!_is_active(), TRANSFER_MODE_RELIABLE, "The multiplayer instance isn't currently active.");
+	ERR_FAIL_COND_V(incoming_packets.size() == 0, TRANSFER_MODE_RELIABLE);
+
+	return incoming_packets.front()->get().transfer_mode;
 }
 
 int SteamMultiplayerPeer::get_packet_channel() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_packet_channel not yet implemented");
-	return -1;
+	ERR_FAIL_COND_V_MSG(!_is_active(), 1, "The multiplayer instance isn't currently active.");
+	ERR_FAIL_COND_V(incoming_packets.size() == 0, 1);
+	int ch = incoming_packets.front()->get().channel;
+	// if (ch >= SYSCH_MAX) { // First 2 channels are reserved.
+	// 	return ch - SYSCH_MAX + 1;
+	// }
+	return 0;
 }
 
 void SteamMultiplayerPeer::disconnect_peer(int p_peer, bool p_force) {
@@ -51,8 +97,7 @@ void SteamMultiplayerPeer::disconnect_peer(int p_peer, bool p_force) {
 }
 
 bool SteamMultiplayerPeer::is_server() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::is_server not yet implemented");
-	return false;
+	return unique_id == 1;
 }
 
 void SteamMultiplayerPeer::poll() {
@@ -64,13 +109,11 @@ void SteamMultiplayerPeer::close() {
 }
 
 int SteamMultiplayerPeer::get_unique_id() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_unique_id not yet implemented");
-	// ERR_FAIL_COND_V_MSG(!_is_active(), 0, "The multiplayer instance isn't currently active.");
+	ERR_FAIL_COND_V_MSG(!_is_active(), 0, "The multiplayer instance isn't currently active.");
 	return unique_id;
 }
 
 SteamMultiplayerPeer::ConnectionStatus SteamMultiplayerPeer::get_connection_status() const {
-	ERR_PRINT("ERROR:: SteamMultiplayerPeer::get_connection_status not yet implemented");
 	return connection_status;
 }
 
--- steam_multiplayer_peer.h
+++ steam_multiplayer_peer.h
@@ -1,11 +1,20 @@
 #ifndef STEAM_MULTIPLAYER_PEER_H
 #define STEAM_MULTIPLAYER_PEER_H
 
+
+
+// Include Godot headers
 #include "scene/main/multiplayer_peer.h"
 
+//Steam APIs
+#include "godotsteam.h"
+#include "core/os/os.h"
 //MultiplayerPeer
 class SteamMultiplayerPeer : public MultiplayerPeer {
 	GDCLASS(SteamMultiplayerPeer, MultiplayerPeer);
+
+protected:
+	static void _bind_methods();
 
 public:
 
@@ -26,9 +35,38 @@ public:
 	virtual int get_unique_id() const override;
 	virtual ConnectionStatus get_connection_status() const override;
 
-private:
-	uint32_t unique_id = 0;
+
+// all SteamGodot from here on down
+
+public:
+	enum LOBBY_TYPE {
+		PRIVATE = ELobbyType::k_ELobbyTypePrivate,
+		FRIENDS_ONLY = ELobbyType::k_ELobbyTypeFriendsOnly,
+		PUBLIC = ELobbyType::k_ELobbyTypePublic,
+		INVISIBLE = ELobbyType::k_ELobbyTypeInvisible,
+		PRIVATE_UNIQUE = ELobbyType::k_ELobbyTypePrivateUnique,
+	};
+
+	enum CHAT_CHANGE {
+		ENTERED = k_EChatMemberStateChangeEntered,
+		LEFT = k_EChatMemberStateChangeLeft,
+		DISCONNECTED = k_EChatMemberStateChangeDisconnected,
+		KICKED = k_EChatMemberStateChangeKicked,
+		BANNED = k_EChatMemberStateChangeBanned
+	};
+
+	enum LOBBY_STATE {
+		NOT_CONNECTED,
+		HOST_PENDING,
+		HOST,
+		CLIENT_PENDING,
+		CLIENT
+	} lobbyState = LOBBY_STATE::NOT_CONNECTED;
+
+	int32_t target_peer = -1;
+	int32_t unique_id = -1;
 	ConnectionStatus connection_status;
+
 	struct Packet {
 		//  *packet = nullptr;
 		int from = 0;
@@ -38,9 +76,12 @@ private:
 	Packet current_packet;
 	List<Packet> incoming_packets;
 
+	_FORCE_INLINE_ bool _is_active() const { return lobbyState != LOBBY_STATE::NOT_CONNECTED; }
+
 public:
 	Error create_server(int max_players);
 	Error create_client();//todo function signature here!
+	
 };
 
 #endif
